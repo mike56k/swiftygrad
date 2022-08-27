4d59fa52f347b0ad53808a00ebf50708c6cf59cb
Mikhail <isachenko.mikhail.k@gmail.com>
2022-08-28 00:52:30 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# swiftygrad
+
+A description of this package.
---
+++ Tests/swiftygradTests/swiftygradTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import swiftygrad
+
+final class swiftygradTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(swiftygrad().text, "Hello, World!")
+    }
+}
